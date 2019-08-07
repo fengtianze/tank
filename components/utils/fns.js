35e5c775bd8c03671af7a60b0ca0d3792852d4d8
tzfeng <tzfeng@alauda.io>
2019-08-07 11:58:54 +0800

    refactor: curry fn

--- components/utils/fns.js
+++ components/utils/fns.js
@@ -1,12 +1,24 @@
-const curry = fn => {
-  const caller = (restLength, prevArgs) => {
-    return (...currentArgs) => {
-      return currentArgs.length < restLength
-        ? caller(restLength - currentArgs.length, [...prevArgs, ...currentArgs])
-        : fn(...prevArgs, ...currentArgs)
+// const curry = fn => {
+//   const caller = (restLength, prevArgs) => {
+//     return (...currentArgs) => {
+//       return currentArgs.length < restLength
+//         ? caller(restLength - currentArgs.length, [...prevArgs, ...currentArgs])
+//         : fn(...prevArgs, ...currentArgs)
+//     }
+//   }
+//   return caller(fn.length, [])
+// }
+const curry = (fn, ...args) => {
+  const caller = (...prevArgs) => {
+    return (...restArgs) => {
+      if (prevArgs.length + restArgs.length >= fn.length) {
+        return fn.call(null, ...prevArgs, ...restArgs)
+      } else {
+        return caller(...prevArgs, ...restArgs)
+      }
     }
   }
-  return caller(fn.length, [])
+  return caller(...args)
 }
 
 const compose = (...fns) => {
@@ -34,6 +46,9 @@ const bind = fn => {
     }
   }
 }
+// Function.prototype.bind = (...args) => {
+//   return bind(this)(...args)
+// }
 
 module.exports = {
   curry,
